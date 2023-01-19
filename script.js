d27a18b4a6df68514036b00c187c45bc7e283ecf
pedrohenrikle <pedro.klein.sl@gmail.com>
2023-01-19 14:06:11 -0300

    -m js buffed

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const form = document.querySelector('#form-habits')
+const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
 
 const data = {
-  run: ["01-01", "01-02", "01-06", "01-07", "01-08", "01-09", "01-10"],
+  run: ["01-01", "01-06", "01-07", "01-09"],
+  water: ["01-02", "01-06", "01-07", "01-09", "01-10"],
+  food: ["01-01", "01-02", "01-07", , "01-10"],
+  sleep: ["01-01", "01-02", "01-06", "01-09", "01-10"],
+  studie: ["01-01", "01-06", "01-07", "01-09"],
+  training: ["01-01", "01-02", "01-07", "01-09", "01-10"],
 }
 
 nlwSetup.setData(data)
